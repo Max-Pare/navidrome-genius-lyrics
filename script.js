682d6f3bb32df14cfd6742cefc557afdb788078d
Max Pare <maxparegd@gmail.com>
2024-12-12 14:30:05 +0100

    made consistent with GreasyFork repo, changed license to MIT

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         NavidromeGeniusLyrics
 // @namespace    http://tampermonkey.net/
-// @version      1.0.1
+// @version      1.0.2
 // @description  Change the semi-useless lyrics button to a GeniusLyrics search
 // @author       Max-Pare
 // @match        http*://192.168.1.100:4533/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=navidrome.org
 // @grant        none
+// @license MIT
 // ==/UserScript==
 // !!! YOU NEED TO CHANGE THE "@match" PROPERTY OF THE SCRIPT TO YOUR ACTUAL SERVER FOR THIS TO EVEN ATTEMPT TO WORK !!!
 // e.g.: @match http*://192.168.1.34:4533/* or http*://music.mydomain.com/* or http*://mydomain.com/music/*
