e7825e6b3178d49ad7bda09f3c884cdc0e0bfcde
Max Pare <maxparegd@gmail.com>
2024-12-12 14:25:30 +0100

    refactored and added versioning

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name         NavidromeGeniusLyrics
 // @namespace    http://tampermonkey.net/
-// @version      2024-12-12
+// @version      1.0.1
 // @description  Change the semi-useless lyrics button to a GeniusLyrics search
-// @author       gregleg
-// @match        http*://YOUR_SERVER_IP:YOUR_NAVIDROME_PORT_OR_WHATEVER/*
+// @author       Max-Pare
+// @match        http*://192.168.1.100:4533/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=navidrome.org
 // @grant        none
 // ==/UserScript==
 // !!! YOU NEED TO CHANGE THE "@match" PROPERTY OF THE SCRIPT TO YOUR ACTUAL SERVER FOR THIS TO EVEN ATTEMPT TO WORK !!!
+// e.g.: @match http*://192.168.1.34:4533/* or http*://music.mydomain.com/* or http*://mydomain.com/music/*
 
 const geniusQueryBase = 'https://genius.com/search?q='
 const observer = new MutationObserver(callback)
@@ -18,29 +19,26 @@ function callback () {
     'use strict';
     observer.disconnect()
     let playBar = document.querySelector('section.panel-content')
-    if(playBar) {
-        let controls = document.querySelector('div.player-content')
-        if (!controls) return
-        let newLyricsButton = controls.querySelector('span.group.lyric-btn')
-        if (!newLyricsButton) return
-        newLyricsButton.removeAttribute('onClick')
-        newLyricsButton.addEventListener('click', function(event) {
-            event.preventDefault();
-            event.stopImmediatePropagation();
-            let artistElem = document.querySelector('span.songArtist')
-            if(!artistElem) return
-            let songElem = document.querySelector('span[class*="songTitle"]')
-            if(!songElem) return
-            let artist = artistElem.textContent;
-            let song = songElem.textContent;
-            let url = geniusQueryBase + artist + ' - ' + song
-            window.open(url, '_blank').focus();
-            console.log(url)
-        });
+    if(!playBar) { observer.observe(document.body, config); return }
 
+    let controls = document.querySelector('div.player-content')
+    if (!controls) return
 
-        observer.observe(playBar, config)
-        return
-    }
-    observer.observe(document.body, config)
+    let newLyricsButton = controls.querySelector('span.group.lyric-btn')
+    if (!newLyricsButton) return
+
+    newLyricsButton.removeAttribute('onClick')
+    newLyricsButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        event.stopImmediatePropagation();
+        let artistElem = document.querySelector('span.songArtist')
+        if(!artistElem) return
+        let songElem = document.querySelector('span[class*="songTitle"]')
+        if(!songElem) return
+        let artist = artistElem.textContent;
+        let song = songElem.textContent;
+        let url = geniusQueryBase + artist + ' - ' + song
+        window.open(url, '_blank').focus();
+    })
+    observer.observe(playBar, config)
 }
